2494e8a7b70614d44adbbe6a6ce33171417336f7
chyams9 <hyamscm@g.cofc.edu>
2021-09-16 15:19:13 -0400

    completed lab 2!

--- labs/lab3/lab3.py
+++ labs/lab3/lab3.py
@@ -1,4 +1,25 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Chris Hyams
+lab3.py
 """
+
+def average():
+    number_of_grades = (eval(input("Enter the number of grades: ")))
+    acc = 0
+    for x in range(0,number_of_grades):
+        grade = eval(input("Enter your grade on HW " + str(x + 1)))
+        acc = acc + grade
+    print(acc / number_of_grades)
+
+def tip_jar():
+    acc = 0
+    for i in range(5):
+        donation = eval(input("Enter your donation amount: "))
+    print(acc / donation)
+
+
+def newton():
+    x = eval(input("Enter x: "))
+    approx = eval(input("Enter the amount
+
+
